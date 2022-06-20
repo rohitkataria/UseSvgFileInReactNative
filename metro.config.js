6ec035fef5d38c8e39587f4858df12d5327e88ea
rohitkataria <rohitkataria0143@gmail.com>
2022-06-20 12:26:41 +0530

    refactor mtro file

--- metro.config.js
+++ metro.config.js
@@ -4,26 +4,28 @@
  *
  * @format
  */
-const {getDefaultConfig} = require('metro-config');
 
-module.exports = (async () => {
-  const {
-    resolver: {sourceExts, assetExts},
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true,
-        },
-      }),
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
+ const { getDefaultConfig } = require("metro-config");
 
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();
+ module.exports = (async () => {
+   const {
+     resolver: { sourceExts, assetExts }
+   } = await getDefaultConfig();
+   return {
+     // projectRoot: path.resolve(__dirname, '../../'),
+     transformer: {
+       getTransformOptions: async () => ({
+         transform: {
+           experimentalImportSupport: false,
+           inlineRequires: false
+         }
+       }),
+       babelTransformerPath: require.resolve("react-native-svg-transformer")
+     },
+     resolver: {
+       assetExts: assetExts.filter(ext => ext !== "svg"),
+       sourceExts: [...sourceExts, "svg"]
+     }
+   };
+ })();
+ 
